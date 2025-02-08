6f78b133fb92a87e4ae4d938eb02b02939b61f3e
kelvinkapuya <kapuyakelvin18.kk@gmail.com>
2025-02-08 03:20:20 -0800

    mm

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ def create_database():
                                                                 cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
                                                                             print("Database 'alx_book_store' created successfully!")
 
-                                                                                except Error as e:
+                                                                                except mysql.connector.Error as e:  # Specific error handling for MySQL
                                                                                             print(f"Error: {e}")
 
                                                                                                 finally:
